89d8a40d9def4dc88f226f8b28d677495933f863
Poleric <nixonleongkb@gmail.com>
2023-07-27 23:00:16 +0800

    refactor

--- gpa-calculator/student.c
+++ gpa-calculator/student.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <student.h>
 
-int filter_sem_courses(int sem, struct Course* courses, size_t courses_len, struct Course* buff) {
+int filter_sem_courses(int sem, Course* courses, size_t courses_len, Course* buff) {
 	int j = 0;  // increments when adding element into dest
 	for (int i = 0; i < courses_len; i++) {
 		if (courses[i].sem == sem) {
