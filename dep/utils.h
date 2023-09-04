d0a19b6d25d3867acb8d57a940b58b327be0432e
Poleric <nixonleongkb@gmail.com>
2023-09-04 11:38:44 +0800

    add logging macros

--- dep/utils.h
+++ dep/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <student.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #ifndef UTILS_H
@@ -10,5 +11,8 @@ int is_char_in(char character, char* character_arr, size_t arr_len);
 
 int filter_sem_courses(int sem, Course** pCourses, size_t course_len, Course** buff);
 Course* get_course_with_id(char* course_code, Course** pCourse, size_t course_len);
+
+#define cannot_alloc(ptr) (ptr == NULL)
+#define log_alloc_error(module_name, variable_name) fprintf(stderr, "%s: Allocation error. Out of memory when allocating %s", module_name, variable_name)
 
 #endif
