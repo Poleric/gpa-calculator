11be5097bbb1ddca9fd93e721a02554378dc4d63
Poleric <nixonleongkb@gmail.com>
2023-09-05 15:32:24 +0800

    Fix text and background color
    Also added footer

--- dep/gui.h
+++ dep/gui.h
@@ -23,8 +23,9 @@ typedef struct FieldData {
     int number_of_rows;
 } FieldData;
 
-int admin_menu();
+int student_list_menu();
 void write_headers();
+void write_footer();
 int update_student_list_window(int current_row);
 int write_student_list_window(int current_row);
 int init_field_data(int max_width, int max_height, int max_sem);
@@ -32,6 +33,9 @@ int init_rows(sqlite3* db);
 static inline int truncate_str(char* string, size_t len);
 static inline void wprintw_center(WINDOW* win, int width, char* format, ...);
 static inline void wprintw_header(WINDOW* win, bool standout);
+static inline void wprintw_footer(WINDOW* win, bool standout);
+static inline void wstandout_line(WINDOW* win, int row, int color_pair);
+void standout_sorted_header(int sort_mode, int color_pair);
 int free_row(RowData* pRow);
 int free_rows();
 #define center_horizontal(length, field_width) ((int)(field_width - length)/2)
